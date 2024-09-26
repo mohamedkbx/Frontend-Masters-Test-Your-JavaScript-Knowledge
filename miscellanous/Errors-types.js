2b1a7f3c96b48dc1d7c7c4de842c726902978944
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-09-26 20:58:22 +0300

    feat: add errors-types

---
+++ miscellanous/Errors-types.js
@@ -0,0 +1,21 @@
+/* 
+new Array(-1)
+ 
+25.toString()
+ 
+[].reduce((acc, cur) => acc + cur);
+ 
+const { a: b = c } = { a: undefined, c: 12 };
+
+const a = { b: 1 }; console.log((a ?? 1) || b);
+
+
+
+ A ReferenceError
+ B TypeError
+ C SyntaxError
+ D RangeError
+ E No error gets thrown
+
+
+*/

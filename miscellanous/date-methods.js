8278c4ca34d222853973c71d9144c6c7fa9347b9
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-09-26 21:12:20 +0300

    feat: default parameters

---
+++ miscellanous/date-methods.js
@@ -0,0 +1,22 @@
+toDateString(); //  Sun Nov 19 2023
+
+toISOString(); //  2023-11-19T23:11:08.263Z
+
+toLocaleDateString(); //11/19/2023
+
+toUTCString(); //Sun, 19 Nov 2023 21:11:08 GMT
+
+toString(); //(Central European Standard Time) Sun, 19 Nov 2023 21:11:08 GMT
+
+/*
+  Sun Nov 19 2023
+
+  2023-11-19T23:11:08.263Z
+
+  Sun Nov 19 2023 22:11:08 GMT+0100 
+
+  (Central European Standard Time)
+  Sun, 19 Nov 2023 21:11:08 GMT
+
+  11/19/2023
+*/
